e06506ab726b20f522938177aee0d87bb3c7dbcf
kroaix <kroaix@gmail.com>
2020-09-08 17:20:36 -0400

    Searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,24 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        #go to the middle
+        middle = (low + high) // 2
+        #is middle less than the target
+        #if less, eliminate right hand side
+        if target < arr[middle]:
+            #high is now the middle
+            high = middle - 1
+        #is middle more than the target
+        #if more, eliminate left side
+        elif target > arr[middle]:
+            #low is now middle
+            low = middle + 1
+        
+        else: 
+            return middle
 
     return -1  # not found
